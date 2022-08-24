005b54d21fe74bf2dd677b16573f8b66968cfd14
Cassius Jonus <vegetafan5@gmail.com>
2022-08-24 16:28:36 -0500

    Implemented the Models module

--- src/Utils/Models.hs
+++ src/Utils/Models.hs
@@ -1,3 +1,32 @@
+{-| This module contains data types that model a store product and a store.
+-}
 module Utils.Models
-    () where
+    ( Product(..)
+    , Store(Amazon, Walmart)
+    , getBaseSearchUrl
+    ) where
+
+
+
+
+-- |a representation of a store product
+data Product = Product
+    { name      :: String
+    , price     :: Double
+    , url       :: String
+    , storeName :: Store
+    }
+
+-- | Describes the possible stores that can be scraped for results of a user's
+-- search request
+data Store = Amazon | Walmart
+
+{- |Returns the base url used by an online store to search for a product given
+a 'Store
+This will be used to scrape the store's website for the lowest price of a
+product based on a user's search terms.
+-}
+getBaseSearchUrl :: Store -> String
+getBaseSearchUrl Amazon  = "https://www.amazon.com/s?k="
+getBaseSearchUrl Walmart = "https://walmart.com/search?q="
 
